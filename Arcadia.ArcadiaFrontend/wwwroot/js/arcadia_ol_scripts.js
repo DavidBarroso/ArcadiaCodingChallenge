38be15cadc0154a5e5901466dd9adc632fb13003
David Barroso <david.barroso@gmail.com>
2021-07-24 01:20:53 +0200

    [#11]First complete version of map viewer [#17][#18] Fixed bug showing the airports in map viewer

--- Arcadia.ArcadiaFrontend/wwwroot/js/arcadia_ol_scripts.js
+++ Arcadia.ArcadiaFrontend/wwwroot/js/arcadia_ol_scripts.js
@@ -1,74 +1,76 @@
 ﻿//Constructor
-var olViewer = function () {
+var olViewer = (function () {
     var map = null;
     var vectorLayer = null;
-    var vectorSource = null;
     var controlFeatures = null;
-};
+});
 //Init map function
 olViewer.prototype.Init = (function (targetId, items) {
     //Save airports
     controlFeatures = items;
 
-    //Vector layer
-    vectorSource = new ol.source.Vector();
-    vectorLayer = new ol.layer.Vector({
-        source: vectorSource,
-        //style: new ol.style.Style({
-        //    fill: new ol.style.Fill({ color: '#8CB2FF' }),
-        //    stroke: new ol.style.Stroke({ color: '#306AFF', width: 1 }),
-        //    zIndez: 99999999
-        //})
-        style: new ol.style.Style({
-            image: new ol.style.Icon({
-                anchor: [0.5, 46],
-                anchorXUnits: 'fraction',
-                anchorYUnits: 'pixels',
-                src: 'https://www.iconpacks.net/icons/2/free-icon-airport-location-2959.png',
-            })
-        })
-    });
+    //Init layer
+    vectorLayer = null;
 
     //Load map viewer
     map = new ol.Map({
         target: targetId,
-        layers: [
-            vectorLayer
-            , new ol.layer.Tile({ source: new ol.source.OSM() })
-        ],
+        layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
         view: new ol.View({
-            //projection: 'EPSG:4326',
+            projection: 'EPSG:3857',
             center: [0, 0],
             zoom: 2
         })
     });
 });
 //Add arrival airport and departures airport to vector layer
-olViewer.prototype.AddFeatures = (function (jsonFeatures) {
+olViewer.prototype.AddVectorLayer = (function (jsonFeatures) {
+    this.ClearFeatures();
     if (jsonFeatures == null || jsonFeatures.length < 1)
         return;
+    
+    var features = [];
 
     //arrival feature
     var arrivalFeature = this.CreateFeature(this.GetControlFeature("icao", jsonFeatures[0].estArrivalAirport));
     arrivalFeature.setStyle(new ol.style.Style({
-        fill: new ol.style.Fill({ color: '#FF0000' }),
-        stroke: new ol.style.Stroke({ color: '#306AFF', width: 1 })
+        image: new ol.style.Circle({
+            fill: new ol.style.Fill({ color: '#4EE084' }),
+            stroke: new ol.style.Stroke({ color: '#008236', width: 1 }),
+            radius: 5
+        }),
     }));
-    vectorSource.addFeature(arrivalFeature);
+    arrivalFeature.setId('arrivalFeature');
+    features.push(arrivalFeature);
 
     //departure features
-    $.each(jsonFeatures, (index, value) => {
+    var index = 0;
+    jsonFeatures.forEach((value) => {
         if (value != null) {
             var departureJSONObj = this.GetControlFeature("icao", value.estDepartureAirport);
             if (departureJSONObj != null) {
-                var departureFeature = this.GetControlFeature(departureJSONObj);
-                try {
-                    vectorSource.addFeature(departureFeature);
-                } catch {
-                }
+                var departureFeature = this.CreateFeature(departureJSONObj);
+                departureFeature.setId('departureFeature' + index);
+                features.push(departureFeature);
+                index++;
             }
         }
     });
+
+    //Vector layer
+    var vectorSource = new ol.source.Vector({ features: features });
+    vectorLayer = new ol.layer.Vector({
+        source: vectorSource,
+        style: new ol.style.Style({
+            image: new ol.style.Circle({
+                fill: new ol.style.Fill({ color: '#8CB2FF' }),
+                stroke: new ol.style.Stroke({ color: '#306AFF', width: 1 }),
+                radius: 5
+            }),
+        })
+    });
+    map.addLayer(vectorLayer);
+    var test = vectorLayer.getSource().getFeatures();
 });
 //Create ol feature to add to map
 olViewer.prototype.CreateFeature = (function (jsonObj) {
@@ -97,11 +99,9 @@ olViewer.prototype.GetControlFeature = (function (key, value) {
 });
 //Clean all feactures
 olViewer.prototype.ClearFeatures = (function () {
-    vectorLayer.getSource().clear();
-
-    //var features = vectorLayer.getSource().getFeatures();
-    //features.forEach((feature) => {
-    //    vectorLayer.getSource().removeFeature(feature);
-    //});
+    if (vectorLayer != null) {
+        map.removeLayer(vectorLayer);
+        vectorLayer = null;
+    }
 
 });
